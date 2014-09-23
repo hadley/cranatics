2bab0c4778045824708f93823a538b7dc6af2caf
hadley <h.wickham@gmail.com>
2014-09-23 07:51:20 -0500

    Improve visualisations

--- 2-recent.R
+++ 2-recent.R
@@ -5,15 +5,40 @@ library(ggplot2)
 cranatics <- readRDS("cranatics.rds")
 
 # Only look at last six months -------------------------------------------------
-recent <- cranatics %>% filter(week > today() - months(6))
+# And drop Achim since he accepts so few packages
 
+recent <- cranatics %>% 
+  filter(week > today() - months(6), uname != "zeileis")
+
+# Long-term trends are weak
+by_day <- recent %>%
+  group_by(date, uname) %>%
+  tally()
+ggplot(by_day, aes(date, n, colour = uname)) + geom_line()
+ggplot(by_day, aes(date, n, colour = uname)) + 
+  geom_line() + 
+  geom_smooth(size = 1, se = F)
+
+# My hypothesis is that weekday and hour will have largest
+# explanatory power
 by_hour <- recent %>% 
   group_by(wday, hour, uname) %>% 
-  tally() %>%
-  mutate(prop = n / sum(n))
+  tally()
 
-ggplot(by_hour, aes(hour, prop, colour = uname)) + 
+# Add in zeros
+grid <- expand.grid(
+  wday = unique(by_hour$wday),
+  hour = unique(by_hour$hour), 
+  uname = unique(by_hour$uname)
+)
+by_hour <- left_join(grid, by_hour) %>%
+  mutate(n = ifelse(is.na(n), 0, n))  
+
+ggplot(by_hour, aes(hour, n, colour = uname)) + 
   geom_line(size = 1) + 
   facet_wrap(~ wday)
 
-# Need to smooth and add 0s
+# Try a little smoothing
+ggplot(by_hour, aes(hour, n, colour = uname)) + 
+  geom_smooth(size = 1, se = FALSE, method = "loess", span = 0.30) + 
+  facet_wrap(~ wday)
